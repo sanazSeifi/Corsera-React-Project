499e7b7ac820eb84fec2963a1d83d238e3a28527
Sanaz <snz5seifi@gmail.com>
2022-04-26 17:39:22 -0400

    structure the Menu

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Menu from "./components/MenuComponent";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar dark color="primary">
+        <div className="container">
+          <navbarBrand href="/">Ristorant</navbarBrand>
+        </div>
+      </Navbar>
+      <Menu />
     </div>
   );
 }
