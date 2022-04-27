075b49d24da368c307c5412f28340c465983fc7c
Sanaz <snz5seifi@gmail.com>
2022-04-27 16:18:43 -0400

    orginize the dish lists and onClick

--- src/App.js
+++ src/App.js
@@ -1,17 +1,33 @@
-import "./App.css";
 import Menu from "./components/MenuComponent";
+import './App.css';
+import {DISHES} from './shared/dishes';
+import { Component } from "react";
 
-function App() {
+
+class App extends Component {
+
+  constructor(props){
+    super(props);
+    this.state = {
+      dishes: DISHES
+    
+  };
+}
+
+render() {
   return (
     <div className="App">
-      {/* <Navbar dark color="primary">
+      {
+      
+      /* <Navbar dark color="primary">
         <div className="container">
           <navbarBrand href="/">Ristorant</navbarBrand>
         </div>
       </Navbar> */}
-      <Menu />
+      <Menu dishes={this.state.dishes}/>
     </div>
   );
+}
 }
 
 export default App;
